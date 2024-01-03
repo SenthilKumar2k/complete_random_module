a285a27f49676f703c418536d9cf77927da07e5a
SenthilKumar2k <senthil2492000@gmail.com>
2024-01-03 20:43:50 +0530

    basic added

---
+++ random_basic.py
@@ -0,0 +1,5 @@
+import random
+
+print(random.random())        # print random num 0 to 1 in the form of float
+print(random.uniform(1,10))   # print random num 1 to 10 in the form of float
+print(random.)
